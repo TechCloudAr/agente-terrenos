e2112a43f17974586a8a5e6c439185b773438cdc
TechCloudAr <techcloud.ar@gmail.com>
2026-03-31 15:12:08 -0300

    Update process.js

--- api/process.js
+++ api/process.js
@@ -1,5 +1,4 @@
-export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
